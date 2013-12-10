0565eba837ff91e4867f133c8e4d63143e450fa6
Matthew Robinson <omegachysis@outlook.com>
2013-12-09 22:00:53 -0700

    Started python outline script

---
+++ arch-fitness/outline.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def initial_procedure():
+    print ("Explain the pushup and good-form " + \
+           "pushups and explain the purpose of the pushup")
+    input()
+    print ("""Note: please consult a doctor about any medical conditions
+before you begin the program.  I am not responsible for any injuries
+relating to the program neither are any of the constituent creators
+or modifiers of this program responsible for any injuries relating to
+the program here-in provided that those who modified this program
+and/or redistributed it have included this explicit message without
+modification.  By pressing 'continue', you verify that you agree
+to these terms; you also verify that you have consulted with a
+health practitioner about your health and/or have confirmed
+you are physically fit for strenuous exercise.""")
+    input("Continue...")
+    
+initial_procedure()
