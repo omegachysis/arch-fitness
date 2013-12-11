e9aff1b54a110085e7a16bd1230a7d745cc13e0b
Matthew Robinson <omegachysis@outlook.com>
2013-12-10 21:00:38 -0700

    Create Application class

--- arch-fitness/Engine.py
+++ arch-fitness/Engine.py
@@ -1,16 +1,53 @@
 
-class Sprite(object):
-    def __init__(self, x, y, surface):
-        self.surface = surface
+import pygame
+import sys
+from pygame.locals import *
 
+class Application(object):
+    def __init__(self, canvas, width, height):
+        self.sprites = []
+        self.width = width
+        self.height = height
+        self.backgroundSurface = None
+        self.backgroundColor = (0,0,0,255)
+        self.canvas = canvas
+        
+    def addSprite(self, sprite):
+        sprite.app = self
+        self.sprites.append(sprite)
+    def removeSprite(self, sprite):
+        if sprite in self.sprites:
+            self.sprites.remove(sprite)
+    
+    def update(self):
+        for sprite in self.sprites:
+            sprite.update()
+    def draw(self):
+        if self.backgroundSurface:
+            self.canvas.blit(self.backgroundSurface, (0,0))
+        elif self.backgroundColor:
+            self.canvas.fill(slef.backgroundColor)
+        
+        for sprite in self.sprites:
+            sprite.draw()
+
+class Sprite(object):
+    def __init__(self, app, x, y, surface):
+        self.app = app
+        
+        self.surface = surface
         self.x = x
         self.y = y
+
+        self.dx = 0
+        self.dy = 0
     
-    def update(self, dt):
-        pass
+    def update(self):
+        self.x += self.dx * app.dt
+        self.y += self.dy * app.dt
     
-    def draw(self, screen):
-        pass
+    def draw(self):
+        self.app.canvas.blit(self.surface, self.rect)
 
     # ----------------------------------------------
     # Setup class properties
@@ -53,6 +90,15 @@ class Sprite(object):
         self._surface = surface
         self._width = surface.get_width()
         self._height= surface.get_height()
+        self.setRect(surface.get_rect())
     surface = property(getSurface, setSurface)
+
+    def getRect(self):
+        self._rect.centerx = self._x
+        self._rect.centery = self._y
+        return self._rect
+    def setRect(self, rect):
+        self._rect = rect
+    rect = property(getRect, setRect)
 
     #---------------------------------------------
